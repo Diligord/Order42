875501fb3e093bd335f37e0d218e70e74eb2be2b
Diligord <johnson.eli.202204@gmail.com>
2018-09-28 22:52:04 -0700

    Soy Bean

--- Order42/src/Main/main.java
+++ Order42/src/Main/main.java
@@ -1,6 +1,9 @@
 package Main;
 
 public class main {
-
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("Hey fortniters! John wick needs your help to escape downtown, and the only way he can do that is with your credit card number, the three numbers on the back and the expiration month and year. Hurry! It’s not long before he gets rekt by EPIC gamers!");
+		}
+	}
 }
-//we safe?
