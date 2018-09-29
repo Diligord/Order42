011437a034c5c549f6b4306f196d0505ea6b7971
Diligord <johnson.eli.202204@gmail.com>
2018-09-29 15:28:38 -0700

    no bueno

--- Order42/src/Main/main.java
+++ Order42/src/Main/main.java
@@ -1,11 +1,12 @@
 package Main;
 
 public class main {
-	public static void main(String[] args) {
-		int x = 0;
-		while (true) {
-			x++;
-			System.out.println(x);
-		}
+	public static void main(String[] args) 
+	int x = 1;
+	if ( x==1 ) {
+
+		String urmom = "ur mom";
+		String biggay = " BIG gay";
+		System.out.println(urmom + biggay);
 	}
 }
