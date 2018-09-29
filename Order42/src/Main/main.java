ece64076f05575f78b346f13a6d7339181289cd0
BuildTools <unconfigured@null.spigotmc.org>
2018-09-28 22:54:05 -0700

    this is ok

--- Order42/src/Main/main.java
+++ Order42/src/Main/main.java
@@ -2,8 +2,10 @@ package Main;
 
 public class main {
 	public static void main(String[] args) {
+		int x = 0;
 		while (true) {
-			System.out.println("Hey fortniters! John wick needs your help to escape downtown, and the only way he can do that is with your credit card number, the three numbers on the back and the expiration month and year. Hurry! It’s not long before he gets rekt by EPIC gamers!");
+			x++;
+			System.out.println(x);
 		}
 	}
 }
